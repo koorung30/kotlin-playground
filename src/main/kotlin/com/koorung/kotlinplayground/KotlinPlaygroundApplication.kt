ddd71ceb7900fc63535ce847271a6544dcb81773
KoorunG <onlyzion10@naver.com>
2023-08-15 01:53:30 +0900

    feat: MainApplication에서 BeanDefinitionDsl을 이용하여 Configuration 기능 추가 (CORS)

--- src/main/kotlin/com/koorung/kotlinplayground/KotlinPlaygroundApplication.kt
+++ src/main/kotlin/com/koorung/kotlinplayground/KotlinPlaygroundApplication.kt
@@ -2,10 +2,31 @@ package com.koorung.kotlinplayground
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.support.beans
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class KotlinPlaygroundApplication
 
 fun main(args: Array<String>) {
-	runApplication<KotlinPlaygroundApplication>(*args)
+
+    // 코틀린에서 BeanDefinitionDsl을 통해 @Configuration의 기능을 바로 사용할 수 있다.
+    val corsBeans = beans {
+        bean {
+            object : WebMvcConfigurer {
+                override fun addCorsMappings(registry: CorsRegistry) {
+                    registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                }
+            }
+        }
+    }
+
+    // addInitializers() 을 통해 스프링부트 앱 실행 전 초기화를 할 수 있다.
+    runApplication<KotlinPlaygroundApplication>(*args) {
+        addInitializers(corsBeans)
+    }
 }
