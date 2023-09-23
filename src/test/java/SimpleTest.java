38c72b19936e856ae2c68be9a79d4d6a4b77fdd1
KoorunGG <onlyzion10@naver.com>
2023-09-23 14:14:49 +0900

    feat: Okky(https://okky.kr/questions/1470236)에 올라온 코테문제 풀이 코드 (Java코드와 Kotlin 코드 작성하여 비교)

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,11 +1,14 @@
 import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Maps;
 import org.assertj.core.util.introspection.CaseFormatUtils;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
 
 class SimpleTest {
 
@@ -26,7 +29,7 @@ class SimpleTest {
                     });
         }
         // 검증
-        Assertions.assertThat(result).isEqualTo(List.of(1, 2, 3, 4, 5, 7, 10, 1, 2, 3, 4, 1, 2, 3, 1, 2, 2)); // 테스트 통과
+        assertThat(result).isEqualTo(List.of(1, 2, 3, 4, 5, 7, 10, 1, 2, 3, 4, 1, 2, 3, 1, 2, 2)); // 테스트 통과
     }
 
 
@@ -54,5 +57,44 @@ class SimpleTest {
 
         System.out.println(CaseFormatUtils.toCamelCase("\'A\'_IX"));
         System.out.println("after :::: " + newResult);
+    }
+
+    @Test
+    void test3() {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int x = 5;
+        int k = 5;
+
+        // 1. int[] -> List<Integer> 변환
+        List<Integer> toList = Arrays.stream(arr).boxed().toList();
+        // 2. Stream 열기
+        List<Integer> result = toList.stream()
+                // 3. [abs(diff), value] List 생성
+                .map(i -> List.of(Math.abs(i - x), i))
+                // 4. 두 List를 비교하는 Comparator로 정렬
+                .sorted((l1, l2) -> {
+                    // 4-1. abs(diff) 비교조건
+                    int first = l1.get(0) - l2.get(0);
+                    // 4-2. abs(diff)가 같을 경우 value 비교조건
+                    int second = l1.get(1) - l2.get(1);
+                    // abs(diff)가 같을 때 (차이가 0일때) 2번 조건으로, 아니면 1번 조건으로 정렬
+                    return first == 0 ? second : first;
+                })
+                // 5. 상위 5개값 가져오기
+                .limit(5)
+                // 6. List[abs(diff), value] -> List[value]로 변경
+                .map(l -> l.get(1))
+                // 7. 재정렬
+                .sorted()
+                .toList();
+
+        System.out.println("result = " + result);   // [3, 4, 5, 6, 7]
+
+
+//        Map<Integer, Integer> map = Map.of(1, 2);           // key, value 한쌍
+//        Stream<Integer> stream = map.keySet().stream();             // Stream<Integer>
+//        Optional<Integer> min = stream.min((a, b) -> a - b);        // min(또는 max)로 Optional<Integer>
+//        Integer i = min.get();                                      // .get()으로 Optional 제거
+//        System.out.println("min = " + i);                           // "min = 1"
     }
 }
--- src/test/kotlin/com/koorung/kotlinplayground/service/OkkyTest.kt
+++ src/test/kotlin/com/koorung/kotlinplayground/service/OkkyTest.kt
@@ -1,6 +1,9 @@
 package com.koorung.kotlinplayground.service
 
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.math.abs
 
 class OkkyTest {
 
@@ -50,5 +53,32 @@ class OkkyTest {
             }
         }
         print(result)
+    }
+
+    /**
+     * 정렬된 정수형 배열 arr과 정수 k, x가 주어진다.
+     * k는 개수, x는 기준값으로 x와의 절대값 차이 기준으로 가까운 수를 k 개수만큼 정렬된 순서로 출력해야 한다.
+     * 절대값 차이가 같을 경우 숫자가 작은 것이 먼저 출력되도록 한다.
+     */
+    @Test
+    fun test3() {
+
+        val arr = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) // 1..10 배열
+        val x = 5   // 기준값
+        val k = 5   // 개수
+
+        val result = arr
+            .map { abs(it - x ) to it }              // 1. 각 배열의 값을 x로 뺀 절대값을 key, 원래값을 value로 하는 Map 생성
+            .sortedWith(
+                compareBy( { it.first }, { it.second }) // 2. key로 우선 정렬하고 동일하다면 value로 정렬
+            )
+            .slice(0 until k)             // 3. k 개수만큼 슬라이스
+            .map { it.second }
+            .sorted()
+            .toIntArray()
+
+        println(arr.toList())
+        println(result.toList())
+        assertThat(result).isEqualTo(intArrayOf(3, 4, 5, 6, 7))
     }
 }
